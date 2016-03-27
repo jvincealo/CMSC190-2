6b0d06d429ee2df3c0faea79e18d76bec712fff0
jvincealo <jvincealo@gmail.com>
2016-03-27 21:36:20 +0800

    Added feedback modal, js for switch modal, modified some css and fonts

--- js/modal.js
+++ js/modal.js
@@ -2,3 +2,8 @@ $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
 });
+
+function switchModal(){
+	$('#modal-feedback').closeModal();
+	$('#modal-login').openModal();
+}

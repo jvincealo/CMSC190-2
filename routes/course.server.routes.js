835dafa58bfbde7dad75c8099af94520b3d6b849
emmanueldunan <dunan.emmanuel@gmail.com>
2016-04-28 01:20:27 +0800

    List all courses in drop down

--- routes/course.server.routes.js
+++ routes/course.server.routes.js
@@ -2,4 +2,5 @@ var courses = require('../controllers/course.server.controller.js');
 
 module.exports = function(app) {
     app.route('/courses/bulk').get(courses.add);
+    app.route('/courses').get(courses.list);
 };
